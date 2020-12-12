63a7933a7d49a704af6f64a555746925f525ef29
Mahmoud Elsayed <mahmoud.elsayed@simon-kucher.com>
2020-12-12 21:26:04 +0200

    finish all feature for the first and third form then add the sorting button on table component

--- src/app/shared-components/table-list/table-list.component.ts
+++ src/app/shared-components/table-list/table-list.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit, Input} from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
 
 @Component({
   selector: 'app-table-list',
@@ -11,6 +12,19 @@ export class TableListComponent implements OnInit {
   @Input() notificationStatus;
   @Input() editActionsStatus;
   step;
+  sortByForRequestNumber = [
+    {
+      id: 'descending',
+      value: 'Descending '
+    },
+    {
+      id: 'ascending',
+      value: 'Ascending '
+    }
+  ];
+  filterByStatus = ['all', 'In Progress', 'starting'];
+  dataAfterFilters = [];
+  sortStatus = false;
 
   constructor() {
   }
@@ -22,4 +36,21 @@ export class TableListComponent implements OnInit {
     this.step = index;
   }
 
+  sortBy(status, columnName) {
+    this.sortStatus = !status;
+    if (!this.sortStatus) {
+      this.dataAfterFilters = this.data.sort((a, b) => (a[columnName] > b[columnName]) ? 1 : -1);
+    } else if (this.sortStatus) {
+      this.dataAfterFilters = this.data.sort((a, b) => (a[columnName] < b[columnName]) ? 1 : -1);
+    }
+  }
+
+  filterForStatus(event) {
+    const typeOfStatus = event.value;
+    if (typeOfStatus !== 'all') {
+      this.dataAfterFilters = this.data.filter(x => x.status === typeOfStatus);
+    } else {
+      this.dataAfterFilters = this.data;
+    }
+  }
 }
