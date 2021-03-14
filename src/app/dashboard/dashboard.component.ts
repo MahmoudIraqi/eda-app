41953e09db510b0e7001ac201e6680dfc1606a09
Mahmoud Elsayed <mahmoud.elsayed@simon-kucher.com>
2021-03-14 20:08:33 +0200

    apply data in the chart

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -40,11 +40,38 @@ export class DashboardComponent implements OnInit {
   ];
   @ViewChild('count', {static: true}) numberCount: ElementRef;
   private data = [
-    {"Framework": "Vue", "Stars": "166443", "Released": "2014"},
-    {"Framework": "React", "Stars": "150793", "Released": "2013"},
-    {"Framework": "Angular", "Stars": "62342", "Released": "2016"},
-    {"Framework": "Backbone", "Stars": "27647", "Released": "2010"},
-    {"Framework": "Ember", "Stars": "21471", "Released": "2011"},
+    {
+      title: 'Notification',
+      value: 1000,
+    },
+    {
+      title: 'Re-Notification',
+      value: 800,
+    },
+    {
+      title: 'Tell & Do Variation',
+      value: 600,
+    },
+    {
+      title: 'Do & Tell Variation',
+      value: 400,
+    },
+    {
+      title: 'Do & Tell Variation',
+      value: 200,
+    },
+    {
+      title: 'Inspection',
+      value: 100,
+    },
+    {
+      title: 'Custom Release',
+      value: 50,
+    },
+    {
+      title: 'General Enquiries',
+      value: 30,
+    }
   ];
   private svg;
   private margin = 50;
@@ -80,47 +107,47 @@ export class DashboardComponent implements OnInit {
   }
 
   private createSvg(): void {
-    this.svg = d3.select("svg")
-      .append("svg")
-      .attr("width", this.width + (this.margin * 2))
-      .attr("height", this.height + (this.margin * 2))
-      .append("g")
-      .attr("transform", "translate(" + this.margin + "," + this.margin + ")");
+    this.svg = d3.select('svg')
+      .append('svg')
+      .attr('width', this.width + (this.margin * 2))
+      .attr('height', this.height + (this.margin * 2))
+      .append('g')
+      .attr('transform', 'translate(' + this.margin + ',' + this.margin + ')');
   }
 
   private drawBars(data: any[]): void {
     // Create the X-axis band scale
     const x = d3.scaleBand()
       .range([0, this.width])
-      .domain(data.map(d => d.Framework))
+      .domain(data.map(d => d.title))
       .padding(0.2);
 
     // Draw the X-axis on the DOM
-    this.svg.append("g")
-      .attr("transform", "translate(0," + this.height + ")")
+    this.svg.append('g')
+      .attr('transform', 'translate(0,' + this.height + ')')
       .call(d3.axisBottom(x))
-      .selectAll("text")
-      .attr("transform", "translate(-10,0)rotate(-45)")
-      .style("text-anchor", "end");
+      .selectAll('text')
+      .attr('transform', 'translate(-10,0)rotate(-45)')
+      .style('text-anchor', 'end');
 
     // Create the Y-axis band scale
     const y = d3.scaleLinear()
-      .domain([0, 200000])
+      .domain([0, 2000])
       .range([this.height, 0]);
 
     // Draw the Y-axis on the DOM
-    this.svg.append("g")
+    this.svg.append('g')
       .call(d3.axisLeft(y));
 
     // Create and fill the bars
-    this.svg.selectAll("bars")
+    this.svg.selectAll('bars')
       .data(data)
       .enter()
-      .append("rect")
-      .attr("x", d => x(d.Framework))
-      .attr("y", d => y(d.Stars))
-      .attr("width", x.bandwidth())
-      .attr("height", (d) => this.height - y(d.Stars))
-      .attr("fill", "#d04a35");
+      .append('rect')
+      .attr('x', d => x(d.title))
+      .attr('y', d => y(d.value))
+      .attr('width', x.bandwidth())
+      .attr('height', (d) => this.height - y(d.value))
+      .attr('fill', '#d04a35');
   }
 }
