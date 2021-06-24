432fae72cf7ec4dc72675e7b8fad81e7b3daa96f
Mahmoud Elsayed <mahmoud.elsayed@simon-kucher.com>
2021-06-24 12:03:59 +0200

    edit the lokkups

--- src/app/product-request-form/product-request-form.component.ts
+++ src/app/product-request-form/product-request-form.component.ts
@@ -79,6 +79,7 @@ export class ProductRequestFormComponent implements OnInit, OnChanges, AfterView
   @ViewChild('packagingModal') modalTemplate: TemplateRef<any>;
   @ViewChild('detailedModal') modalDetailedTemplate: TemplateRef<any>;
   @ViewChildren(MatAutocompleteTrigger) triggerCollection: QueryList<MatAutocompleteTrigger>;
+  @ViewChild('autoIngrediant', {static: false}) autoIngrediantInput: ElementRef;
   isDraft: boolean = false;
   detailsListTable = {
     tableHeader: ['Colour', 'Fragrance', 'Flavor', 'BarCode', 'Actions'],
@@ -414,6 +415,8 @@ export class ProductRequestFormComponent implements OnInit, OnChanges, AfterView
     this._subscribeToClosingActions('storagePlace', this.filteredOptionsForStoragePlace);
     this._subscribeToClosingActionsForPackagingFormArray('unitOfMeasure', this.filteredOptionsForUnitOfMeasure);
     this._subscribeToClosingActionsForPackagingFormArray('typeOfPackaging', this.filteredOptionsForTypeOfPackaging);
+
+    console.log('12341234', this.autoIngrediantInput); //.nativeElement.addEventListener('scroll', this.onScrollFunction(event))
   }
 
   ngOnDestroy() {
@@ -997,8 +1000,7 @@ export class ProductRequestFormComponent implements OnInit, OnChanges, AfterView
       return formControlValue.valueChanges
         .pipe(
           startWith(''),
-          debounceTime(300),
-          map(state => state ? this.filterInsideList(whichLookup, state, list) : list.slice())
+          map(state => state ? this.filterInsideList(whichLookup, state, list).slice(0, 1000) : list.slice(0, 1000))
         );
     }
   }
@@ -1502,5 +1504,9 @@ export class ProductRequestFormComponent implements OnInit, OnChanges, AfterView
     this.filteredOptionsForStoragePlace = this.filterLookupsFunction('storagePlace', this.regProductForAllRequestedType.get('storagePlace'), this.formData.storagePlaceList);
     this.filteredOptionsForUnitOfMeasure = this.filterLookupsFunction('unitOfMeasure', this.regPackagingForProduct.get('unitOfMeasure'), this.formData.unitOfMeasureList);
     this.filteredOptionsForTypeOfPackaging = this.filterLookupsFunction('typeOfPackaging', this.regPackagingForProduct.get('typeOfPackaging'), this.formData.typeOfPackagingList);
+  }
+
+  onScrollFunction(event) {
+
   }
 }
